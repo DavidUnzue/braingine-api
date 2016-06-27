ac19d5fc9a333ba8e81aabf92f8812559e56fb0d
David Unzue <david.unzue@brain.mpg.de>
2016-06-27 12:05:24 +0200

    Added marshmallow json api schemas to models. Updated migrations folder.

--- server/models.py
+++ server/models.py
@@ -2,16 +2,29 @@ from flask import current_app
 from . import db
 import os
 from utils import silent_remove, sha1_string
+from marshmallow_jsonapi import Schema, fields
+
 
 # Define a base model for other models to inherit
 class Base(db.Model):
+    """
+    Base model with attributes common to every model. Other models will inherit from this one.
+    """
 
+    # tell SQLAlchemy not to create a table for a this model
     __abstract__  = True
 
     id            = db.Column(db.Integer, primary_key=True)
     date_created  = db.Column(db.DateTime(timezone=True),  default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime(timezone=True),  default=db.func.current_timestamp(),
                                            onupdate=db.func.current_timestamp())
+
+
+# Marshmallow schema for base model
+class BaseSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    date_created = fields.DateTime()
+    date_modified = fields.DateTime()
 
 
 # Define Experiment model
@@ -45,14 +58,37 @@ class Experiment(Base):
         return '<Experiment %d>' % self.id
 
 
+# Marshmallow schema for experiments
+class ExperimentSchema(BaseSchema):
+    name = fields.Str()
+    exp_type = fields.Str()
+    date = fields.Date()
+    experimenter = fields.Str()
+    species = fields.Str()
+    tissue = fields.Str()
+
+    files = fields.Relationship(
+        related_url='/experiments/{experiment_id}/files',
+        related_url_kwargs={'experiment_id': '<id>'},
+        # Include resource linkage
+        many=True, include_resource_linkage=True,
+        type_='files'
+    )
+
+    class Meta:
+        type_ = 'experiments'
+        strict = True
+
+
 # Define Experiment files model
 class ExperimentFile(Base):
 
     __tablename__ = "experiment_files"
 
     # Attributes
-    uid = db.Column(db.String(40), nullable=True, default='')
-    experiment_id = db.Column(db.Integer, db.ForeignKey("experiments.id", ondelete="CASCADE"))
+    # filename hash string will be generated from file name using SHA1 hashing, see event "hash_before_insert"
+    filename_hash = db.Column(db.String(40), nullable=True, default='')
+    experiment_id = db.Column(db.Integer(), db.ForeignKey("experiments.id", ondelete="CASCADE"))
     file_name = db.Column(db.String(255), nullable=False, default='')
     file_path = db.Column(db.String(255), nullable=False, default='')
     # filesize stored as amount of Bytes
@@ -72,6 +108,21 @@ class ExperimentFile(Base):
 
     def __repr__(self):
         return '<Experiment file %d>' % self.id
+
+
+# Marshmallow schema for experiments
+class ExperimentFileSchema(BaseSchema):
+    file_name = fields.Str()
+    filename_hash = fields.Str()
+    experiment_id = fields.Int()
+    file_path = fields.Str()
+    # python integer type can store very large numbers, there is no toher data type like bigint
+    file_size = fields.Int()
+    file_group = fields.Str()
+
+    class Meta:
+        type_ = 'files'
+        strict = True
 
 
 @db.event.listens_for(ExperimentFile, 'after_delete')
@@ -94,7 +145,7 @@ def remove_directory_after_delete(mapper, connection, target):
 @db.event.listens_for(ExperimentFile, 'before_insert')
 def hash_before_insert(mapper, connection, target):
     """
-    Create a hash out of the filename for use as unique identifier
+    Create a hash string out of the filename for use as file unique identifier
     """
     filename_hash = sha1_string(target.file_name)
-    target.uid = filename_hash
+    target.filename_hash = filename_hash
