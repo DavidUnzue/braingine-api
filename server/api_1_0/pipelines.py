b9054ae3d4b950e1f148270a157b516e531f82cf
David Unzue <david.unzue@brain.mpg.de>
2016-11-11 17:24:48 +0100

    Changed pipeline models

--- server/api_1_0/pipelines.py
+++ server/api_1_0/pipelines.py
@@ -5,7 +5,7 @@ from flask import abort, current_app
 from flask.ext.restful import Resource, reqparse, fields
 
 from server import db
-from server.models.pipeline import Pipeline, PipelineSchema
+from server.models.pipeline import PipelineSchema
 from . import api
 import json
 
@@ -21,39 +21,21 @@ class PipelineListController(Resource):
         # get json files (pipeline definition files) from pipelines folder
         pipeline_files = [f for f in listdir(current_app.config.get('PIPELINES_FOLDER')) if isfile(join(current_app.config.get('PIPELINES_FOLDER'), f)) and f.endswith(".json")]
 
-        # parser.add_argument('page', type=int, default=1, location=['args'])
-        # parsed_args = parser.parse_args()
-        # page = parsed_args['page']
+        pipeline_definition_list = list()
+        for pipleine_file in pipeline_files:
+            with open('{}/{}'.format(current_app.config.get('PIPELINES_FOLDER'), pipleine_file)) as pipeline_definition_file:
+                pipeline_definition = json.load(pipeline_definition_file)
+                pipeline_definition_list.append(pipeline_definition)
 
-        # pagination = Pipeline.query.paginate(page, 5)
-        # pipelines = pagination.items
-        # page_prev = None
-        # if pagination.has_prev:
-        #     page_prev = api.url_for(self, page=page-1, _external=True)
-        # page_next = None
-        # if pagination.has_next:
-        #     page_next = api.url_for(self, page=page+1, _external=True)
-        #
-        # result = pipeline_schema.dump(pipelines, many=True).data
-        result = pipeline_files
+        result = pipeline_schema.dump(pipeline_definition_list, many=True).data
         return result, 200
 
 
 class PipelineController(Resource):
     def get(self, pipeline_filename):
-        # parser.add_argument('pipeline_filename', type=int, default=1, location=['form, json'])
-        # parsed_args = parser.parse_args()
-        # pipeline_filename = parsed_args['pipeline_filename']
 
         with open('{}/{}.json'.format(current_app.config.get('PIPELINES_FOLDER'), pipeline_filename)) as pipeline_definition_file:
             pipeline_definition = json.load(pipeline_definition_file)
 
-        # pipeline_input = pipeline_definition["inputs"][0]
-        # pipeline_output = pipeline_definition["outputs"][0]
-
-        # pipeline = Pipeline.query.get(pipeline_id)
-        # if not pipeline:
-        #     abort(404, "Pipeline {} doesn't exist".format(pipeline_id))
-        # result = pipeline_schema.dump(pipeline_definition).data
-        result = pipeline_definition
+        result = pipeline_schema.dump(pipeline_definition).data
         return result, 200
--- server/models/pipeline.py
+++ server/models/pipeline.py
@@ -1,26 +1,12 @@
-from .base import Base, BaseSchema
-from server import db
-from marshmallow_jsonapi import fields
+from marshmallow_jsonapi import Schema, fields
 
 
-class Pipeline(Base):
-
-    __tablename__ = 'pipelines'
-
-    script = db.Column(db.String(255), nullable=False, default='')
-    definition = db.Column(db.String(255), nullable=False, default='')
-
-    def __init__(self, script, definition):
-        self.script = script
-        self.definition = definition
-
-    def __repr__(self):
-        return '<Pipeline {}>'.format(self.id)
-
-
-class PipelineSchema(BaseSchema):
-    script = fields.Str()
-    definition = fields.Str()
+class PipelineSchema(Schema):
+    id = fields.Str()
+    name = fields.Str()
+    description = fields.Str()
+    command = fields.Str()
+    parameters = fields.Dict()
 
     class Meta:
         type_ = 'pipelines'
