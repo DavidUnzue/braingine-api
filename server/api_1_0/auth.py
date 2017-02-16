dfa969f327e9c5f4de1526d7ed9338b3e3bd7a76
David Unzue <david.unzue@brain.mpg.de>
2017-02-16 11:41:27 +0100

    Added escaping of LDAP username to protect against injections.

--- server/api_1_0/auth.py
+++ server/api_1_0/auth.py
@@ -1,4 +1,5 @@
 import ldap
+from ldap.filter import escape_filter_chars
 from flask import jsonify, make_response, g, current_app
 from flask.ext.httpauth import HTTPBasicAuth
 
@@ -16,7 +17,9 @@ def verify_password(username, password):
     # connect to LDAP server and bind known user
     con = ldap.initialize(current_app.config.get('LDAP_SERVER'), bytes_mode=False)
     con.simple_bind_s(current_app.config.get('LDAP_USERNAME'), current_app.config.get('LDAP_PASSWORD'))
-    # search for authenticating user
+    # escape special chars before filtering to protect against LDAP injection
+    username = escape_filter_chars(username)
+    # search for authenticating user using LDAP filtering
     results = con.search_s(current_app.config.get('LDAP_BASE_DN'), ldap.SCOPE_SUBTREE,                '(&(objectclass=Person)(|(mail={0})(sAMAccountName={0})))'.format(username))
     # if user not found
     if results is None or len(results) <= 0 or password == '':
