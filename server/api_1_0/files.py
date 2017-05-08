4a9982310beaa96e7eba97a25e90d9e2f951a33f
David Unzue <david.unzue@brain.mpg.de>
2017-05-08 16:19:52 +0200

    Added pagination to files resource.

--- server/api_1_0/files.py
+++ server/api_1_0/files.py
@@ -6,13 +6,30 @@ from flask.ext.restful import Resource
 
 from ..models.experiment import ExperimentFile, ExperimentFileSchema
 import json
+from . import api
+from webargs import fields
+from webargs.flaskparser import use_args
 
 experiment_file_schema = ExperimentFileSchema()
 
 
 class FileListController(Resource):
-    def get(self):
-        files = ExperimentFile.query.all()
+    @use_args({
+        'page': fields.Int(missing=1)
+    })
+    def get(self, args):
+        # pagination
+        page = args['page']
+        pagination = ExperimentFile.query.paginate(page, 5, False)
+        files = pagination.items
+        #experiments = Experiment.query.all()
+        page_prev = None
+        if pagination.has_prev:
+            page_prev = api.url_for(self, page=page-1, _external=True)
+        page_next = None
+        if pagination.has_next:
+            page_next = api.url_for(self, page=page+1, _external=True)
+
         result = experiment_file_schema.dump(files, many=True).data
 
         return result, 200
